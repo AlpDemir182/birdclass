27ce9ee187764309b3478235fb4b2d9677c6ce14
AlpDemir182 <hagunenjoyer7@gmail.com>
2024-10-27 12:56:34 +0100

    Add files via upload

---
+++ birdclass.py
@@ -0,0 +1,17 @@
+class Bird():
+    no_wings = 2
+    no_legs = 2
+    no_beak = 1
+    no_eyes = 2
+    
+
+    def chirp(self):
+        print("Chirp! Chirp!")
+
+    def eat_seeds(self):
+        print("eat the seeds")
+
+Woodpecker = Bird()
+Mockingbird = Bird()
+Blue_Jay = Bird()
+Sparrow = Bird()
